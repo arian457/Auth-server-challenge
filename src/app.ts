840e1c9f104d296d1199936979eedf15a0b36c5e
arian457 <ariancejas00@gmail.com>
2021-12-03 15:40:46 -0300

    establish conn to db after sv conn

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import createServer from './utils/createServer';
+import connectdb from './db';
+
+require('dotenv').config();
+
+const { DB_DEV_URI } = process.env;
 
 const server = createServer();
 
 const port = 4000;
 
-server.listen(port, () => console.log(`Running on port ${port}`));
+server.listen(port, async () => {
+  console.log(`Running on port ${port}`);
+  await connectdb(DB_DEV_URI).then((success) => success).catch((error) => console.log('db Connection failed'))
+});
