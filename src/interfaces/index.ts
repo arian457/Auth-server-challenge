0493ad2fcb28ace434267f5cff8d632ea2369c14
arian457 <ariancejas00@gmail.com>
2021-12-03 15:42:29 -0300

    added User interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,75 +1,10 @@
-//enums
-enum Resources {
-  'locations',
-  'episodes',
-  'characters',
+export interface creationUser {
+  userName: string;
+  email: string;
+  password: string;
+  role: string;
 }
-//
-
-//External API's interfaces
-export interface graphqlResponseObject {
-  data: dataObject;
+export interface responseUser {
+  userName: string;
+  email: string;
 }
-export interface resultsObject {
-  results: Array<episodesReqData>;
-}
-export interface episodesReqData {
-  name:string;
-  episode:string;
-  characters:any[]
-}
-interface dataObject {
-  episodes?: resultsObject;
-  locations?: resultsObject;
-  characters?: resultsObject;
-}
-interface originReqResults {
-  name: string;
-}
-export interface charactersName{
-  name:string
-}
-export interface charObject {
-  origin?: originReqResults;
-  location?: object[];
-}
-
-// exercise interfaces
-interface execOneResults {
-  char: string;
-  resource: string;
-  count: number;
-}
-interface execTwoResults {
-  name: string;
-  episode: string;
-  locations: string[];
-}
-interface exerciseResponse {
-  exercise_name: string;
-  in_time: boolean;
-  time: string;
-  results: [execOneResults | execTwoResults];
-}
-export interface charCountResult {
-  char: string;
-  count: number;
-  resource: string;
-}
-export interface episodesLocationResults {
-  name?: string;
-  episode?: string;
-  locations?: string[];
-}
-// end
-
-
-//express custom interfaces
-export interface errorResponse {
-  status: number;
-  message: string;
-}
-export interface apiResponse {
-  body: exerciseResponse;
-}
-// end
